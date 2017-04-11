49019c81509caf2ad442bb1a57aac84bae6c2baf
zhuravlev <i.zhuravlev@ibcico.com>
2017-04-11 14:09:01 +0300

    initial

---
+++ README.markdown
@@ -0,0 +1,47 @@
+# Client logo
+-----------
+
+Version: 1.0
+Author: Илья Журавлёв
+Build Date: 2017-04-11  
+Requirements: Symphony 2.3 or higher
+
+## About
+
+Google reCAPTCHA integration for Symphony CMS
+
+
+## Installation
+
+1. Upload the `google_recaptcha` folder in this archive to your Symphony `extensions` folder.
+
+2. Enable it the usual way.
+
+
+## Usage
+
+1. Get Google reCAPTCHA API keys
+
+2. Go to System > Preferences and enter your reCAPTCHA site/secret API key pair
+
+3. Add the "Google reCAPTCHA" filter rule to your Event via Blueprints > Events
+
+4. Save the Event
+
+5. Paste this snippet before the closing </head> tag on your HTML template:
+
+```HTML    
+<script src='https://www.google.com/recaptcha/api.js'></script>
+```
+
+6. Paste this snippet at the end of the <form> where you want the reCAPTCHA widget to appear:
+
+```HTML    
+<div class="g-recaptcha" data-sitekey="{$google_recaptcha}"></div>
+```
+
+
+## Changelog
+
+### 1.0
+ - Initial release
---
+++ extension.driver.php
@@ -0,0 +1,110 @@
+<?php
+
+	/*
+	Copyight: Ilya Zhuravlev 2017
+	License: MIT, see the LICENCE file
+	*/
+
+	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+
+	Class extension_google_recaptcha extends Extension {
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/system/preferences/',
+					'delegate' => 'AddCustomPreferenceFieldsets',
+					'callback' => 'addCustomPreferenceFieldsets'
+				),
+				array(
+					'page' => '/blueprints/events/edit/',
+					'delegate' => 'AppendEventFilter',
+					'callback' => 'appendEventFilter'
+				),
+				array(
+					'page' => '/blueprints/events/new/',
+					'delegate' => 'AppendEventFilter',
+					'callback' => 'appendEventFilter'
+				),
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'FrontendParamsResolve',
+					'callback' => 'frontendParamsResolve'
+				),
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'EventPreSaveFilter',
+					'callback' => 'eventPreSaveFilter'
+				),
+			);
+		}
+
+		public function uninstall(){
+			Administration::Configuration()->remove('google_recaptcha');
+			Administration::Configuration()->write();
+		}
+
+		public function appendEventFilter($context){
+			$handle = 'google_recaptcha';
+			$selected = (in_array($handle, $context['selected']));
+			$context['options'][] = array(
+				$handle, $selected, General::sanitize('Google reCAPTCHA')
+			);
+		}
+
+		public function addCustomPreferenceFieldsets($context){
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(
+				new XMLElement('legend', 'Google reCAPTCHA')
+			);
+
+			$site_key = Widget::Label(__('Site key'));
+			$site_key->appendChild(Widget::Input(
+				'settings[google_recaptcha][site_key]', General::Sanitize(Administration::Configuration()->get('site_key', 'google_recaptcha'))
+			));
+			$group->appendChild($site_key);
+
+			$site_key = Widget::Label(__('Secret key'));
+			$site_key->appendChild(Widget::Input(
+				'settings[google_recaptcha][secret_key]', General::Sanitize(Administration::Configuration()->get('secret_key', 'google_recaptcha'))
+			));
+			$group->appendChild($site_key);
+
+			$context['wrapper']->appendChild($group);
+		}
+
+		public function frontendParamsResolve($context){
+			$site_key = Symphony::Configuration()->get('site_key', 'google_recaptcha');
+
+			if($site_key){
+				$context['params']['google_recaptcha'] = $site_key;
+			}
+		}
+
+		public function eventPreSaveFilter($context){
+			$url = 'https://www.google.com/recaptcha/api/siteverify';
+			$post = array(
+				'secret' => Symphony::Configuration()->get('secret_key', 'google_recaptcha'),
+				'response' => $_POST['g-recaptcha-response']
+			);
+
+			$curl = curl_init();
+
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+			$result = json_decode(curl_exec($curl));
+
+			curl_close($curl);
+
+			$handle = 'google_recaptcha';
+			if(in_array($handle, (array)$context['event']->eParamFILTERS)){
+				if($result->success != 1){
+					$context['messages'][] = array('google_recaptcha', false, 'Google reCAPTCHA failed');
+				}
+			}
+		}
+	}
